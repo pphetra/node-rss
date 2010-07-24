fc890c3756ec70ff3bfae812e9f1ef49d4712df5
pphetra <pphetra@pphetras-macbook.local>
2010-07-24 17:54:11 +0700

    refactor: support simultaneous fetch.

--- node-rss.js
+++ node-rss.js
@@ -16,83 +16,82 @@
 var sys = require('sys'), http = require('http');
 var xml = require("./node-xml");
 
-// variable for holding the callback function which is passed to the
-// exported function. This callback is passed the articles array
-var callback = function() {};
-
 // The main "meat" of this module - parses an rss feed and triggers
 // the callback when done.
 // using node-xml: http://github.com/robrighter/node-xml
-var parser = new xml.SaxParser(function(cb) {
-    var articles = Array();
-    var current_element = false;
-    var article_count = 0;
-    var in_item = false;
-    var current_chars = '';
+var newParser = function(callback) {
+    var parser = new xml.SaxParser(function(cb) {
+        var articles = Array();
+        var current_element = false;
+        var article_count = 0;
+        var in_item = false;
+        var current_chars = '';
 
 
-    cb.onStartDocument(function() { });
+        cb.onStartDocument(function() { });
 
-    // when finished parsing the RSS feed, trigger the callback
-    cb.onEndDocument(function() {	    
-	callback(articles);
+        // when finished parsing the RSS feed, trigger the callback
+        cb.onEndDocument(function() {
+    	    callback(articles);
+        });
+
+
+        //track what element we are currently in. If it is an <item> this is
+        // an article, add container array to the list of articles
+        cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
+    	current_element = elem.toLowerCase();
+    	if(current_element == 'item' || current_element == 'entry') {
+    	    in_item = true;
+    	    articles[article_count] = Array();
+    	}
+        });
+        // when we are at the end of an element, save its related content
+        cb.onEndElementNS(function(elem, prefix, uri) {
+    	if(in_item) {
+    	    switch(current_element)
+    	    {
+    	    case 'description':
+    	    case 'summary':
+    		articles[article_count][current_element] = current_chars.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    		break;
+    	    case 'content':
+    	    case 'encoded': // feedburner is <content:encoded>, node-xml reads as <encoded>
+    		current_element = 'content';
+    		articles[article_count][current_element] = current_chars.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    		break;
+    	    case 'link':
+    	    case 'title':
+    		articles[article_count][current_element] = current_chars.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
+    		break;
+    	    }
+
+    	    current_element = false;
+    	    current_chars = '';
+    	    if(elem.toLowerCase() == 'item' || elem.toString() == 'entry') {
+    		in_item = false;
+    		article_count ++;
+    	    }
+    	}
+        });
+
+        cb.onCharacters(addContent);
+        cb.onCdata(addContent);
+        function addContent(chars) {
+    	if(in_item) {
+    	    current_chars += chars;
+    	}
+        };
+
+        // @TODO handle warnings and errors properly
+        cb.onWarning(function(msg) {
+    	sys.puts('<WARNING>'+msg+"</WARNING>");
+        });
+        cb.onError(function(msg) {
+    	sys.puts('<ERROR>'+JSON.stringify(msg)+"</ERROR>");
+        });
     });
-
-
-    //track what element we are currently in. If it is an <item> this is
-    // an article, add container array to the list of articles
-    cb.onStartElementNS(function(elem, attrs, prefix, uri, namespaces) {
-	current_element = elem.toLowerCase();
-	if(current_element == 'item' || current_element == 'entry') {
-	    in_item = true;
-	    articles[article_count] = Array();
-	}
-    });
-    // when we are at the end of an element, save its related content
-    cb.onEndElementNS(function(elem, prefix, uri) {
-	if(in_item) {
-	    switch(current_element) 
-	    {
-	    case 'description':
-	    case 'summary':
-		articles[article_count][current_element] = current_chars.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-		break;
-	    case 'content':
-	    case 'encoded': // feedburner is <content:encoded>, node-xml reads as <encoded>
-		current_element = 'content';
-		articles[article_count][current_element] = current_chars.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-		break;
-	    case 'link':
-	    case 'title':
-		articles[article_count][current_element] = current_chars.replace(/^\s\s*/, '').replace(/\s\s*$/, '');
-		break;
-	    }
-	    
-	    current_element = false;
-	    current_chars = '';
-	    if(elem.toLowerCase() == 'item' || elem.toString() == 'entry') {
-		in_item = false;
-		article_count ++;   
-	    }
-	}
-    });
-    
-    cb.onCharacters(addContent);
-    cb.onCdata(addContent);
-    function addContent(chars) {
-	if(in_item) {
-	    current_chars += chars;
-	}
-    };
-
-    // @TODO handle warnings and errors properly
-    cb.onWarning(function(msg) {
-	sys.puts('<WARNING>'+msg+"</WARNING>");
-    });
-    cb.onError(function(msg) {
-	sys.puts('<ERROR>'+JSON.stringify(msg)+"</ERROR>");
-    });
-});
+    return parser;
+}
 
 
 /**
@@ -102,8 +101,7 @@ var parser = new xml.SaxParser(function(cb) {
  * @param cb - callback function to be triggered at end of parsing
  */
 exports.parseFile = function(file, cb) {
-    callback = cb;
-    parser.parseFile(file);
+    newParser(cb).parseFile(file);
 }
 /**
  * parseURL()
@@ -114,7 +112,6 @@ exports.parseFile = function(file, cb) {
  * @TODO - decent error checking
  */
 exports.parseURL = function(url, cb) {
-    callback = cb;
 
     get_rss(url);
     function get_rss(url) {
@@ -142,7 +139,7 @@ exports.parseURL = function(url, cb) {
 		    body += chunk;
 		});
 		response.addListener('end', function() {
-		    parser.parseString(body);
+		    newParser(cb).parseString(body);
 		});
 		break;
 		// redirect status returned
